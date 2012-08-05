d71c2e29f29d8ec776a34e06a88fcfbfcbbb1a2c
Takaaki Sasada <sasada536@gmail.com>
2012-08-06 08:24:03 +0900

    Add firstapp script

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,32 @@
  * GET home page.
  */
 
+var model = require('../model');
+var Post = model.Post;
+
+var items = [
+  { "text": "1st Post." },
+  { "text": "2nd Post." }
+];
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  Post.find({}, function(err, items){
+    res.render('index', { title: 'Entry List', items: items })
+  });
+};
+
+exports.form = function(req, res){
+  res.render('form', { title: 'New Entry' })
+};
+
+exports.create = function(req, res){
+  var newPost = new Post(req.body);
+  newPost.save(function(err) {
+    if (err) {
+      console.log(err);
+      res.redirect('back');
+    } else {
+      res.redirect('/');
+    }
+  });
 };
